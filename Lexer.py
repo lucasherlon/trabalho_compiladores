425fa3146f5d9d88ea306378a8245a80608dce75
lucasherlon <lucasherlondsmc@gmail.com>
2025-01-13 21:37:54 -0300

    refatorando código

--- Lexer.py
+++ Lexer.py
@@ -1,126 +1,78 @@
-import re
-import sys
+import ply.lex as lex
 
-TOKEN_TYPES = {
-    'PROGRAM': r'\b(Program)\b',
-    'TYPE': r'\b(int|float|str|bool)\b',
-    'CONDITIONAL': r'\b(if|else)\b',
-    'LOOP': r'\b(while)\b',
-    'INPUT': r'\b(input)\b',
-    'OUTPUT': r'\b(print)\b',
-    'KEYWORD': r'\b(const|break)\b',
-    'IDENTIFIER': r'[a-zA-Z][a-zA-Z_0-9]*',
-    'NUMBER_INT': r'\b\d+\b',
-    'NUMBER_FLOAT': r'\b\d+\.\d+\b',
-    'STRING': r'"[^"]*"',
-    'OP_SOMA': r'\+',
-    'OP_SUBT': r'-',
-    'OP_MULT': r'\*',
-    'OP_DIV': r'/',
-    'OP_IGUAL': r'==',
-    'OP_ATRIB': r'=',
-    'OP_DIFERENTE': r'!=',
-    'OP_MAIOR': r'>',
-    'OP_MENOR': r'<',
-    'OP_MAIOR_IGUAL': r'>=',
-    'OP_MENOR_IGUAL': r'<=',
-    'PAREN_ESQ': r'\(',
-    'PAREN_DIR': r'\)',
-    'CHAVE_ESQ': r'\{',
-    'CHAVE_DIR': r'\}',
-    'COLCH_ESQ': r'\[',
-    'COLCH_DIR': r'\]',
-    'PONTO_VIRGULA': r';',
-    'VIRGULA': r',',
-    'PONTO': r'\.',
-    'WHITESPACE': r'[ \t]+',
-    'NEWLINE': r'\n',
-    'COMMENT': r'//.*'
-}
-
-showed_types = (
-    'INPUT', 'OUTPUT', 'CONDITIONAL', 'LOOP', 'IDENTIFIER',
-    'OP_SOMA', 'OP_SUBT', 'OP_MULT', 'OP_DIV',
-    'OP_IGUAL', 'OP_DIFERENTE', 'OP_MAIOR', 'OP_MENOR',
-    'OP_MAIOR_IGUAL', 'OP_MENOR_IGUAL', 'PROGRAM', 'OP_ATRIB',
+tokens = (
+    'PROGRAM', 'TYPE', 'CONDITIONAL', 'LOOP', 'INPUT', 'OUTPUT', 'KEYWORD',
+    'IDENTIFIER', 'NUMBER_INT', 'NUMBER_FLOAT', 'STRING',
+    'OP_SOMA', 'OP_SUBT', 'OP_MULT', 'OP_DIV', 'OP_IGUAL', 'OP_ATRIB',
+    'OP_DIFERENTE', 'OP_MAIOR', 'OP_MENOR', 'OP_MAIOR_IGUAL', 'OP_MENOR_IGUAL',
     'PAREN_ESQ', 'PAREN_DIR', 'CHAVE_ESQ', 'CHAVE_DIR',
-    'COLCH_ESQ', 'COLCH_DIR', 'PONTO',
-    'TYPE', 'NUMBER_INT', 'NUMBER_FLOAT', 'STRING'
+    'COLCH_ESQ', 'COLCH_DIR', 'PONTO_VIRGULA', 'VIRGULA', 'PONTO'
 )
 
+# Regras simples baseadas em expressões regulares
+t_PROGRAM = r'\bProgram\b'
+t_TYPE = r'\b(int|float|str|bool)\b'
+t_CONDITIONAL = r'\b(if|else)\b'
+t_LOOP = r'\b(while)\b'
+t_INPUT = r'\b(input)\b'
+t_OUTPUT = r'\b(print)\b'
+t_KEYWORD = r'\b(const|break)\b'
+t_IDENTIFIER = r'[a-zA-Z_][a-zA-Z_0-9]*'
+t_NUMBER_INT = r'\b\d+\b'
+t_NUMBER_FLOAT = r'\b\d+\.\d+\b'
+t_STRING = r'"[^"]*"'
+t_OP_SOMA = r'\+'
+t_OP_SUBT = r'-'
+t_OP_MULT = r'\*'
+t_OP_DIV = r'/'
+t_OP_IGUAL = r'=='
+t_OP_ATRIB = r'='
+t_OP_DIFERENTE = r'!='
+t_OP_MAIOR = r'>'
+t_OP_MENOR = r'<'
+t_OP_MAIOR_IGUAL = r'>='
+t_OP_MENOR_IGUAL = r'<='
+t_PAREN_ESQ = r'\('
+t_PAREN_DIR = r'\)'
+t_CHAVE_ESQ = r'\{'
+t_CHAVE_DIR = r'\}'
+t_COLCH_ESQ = r'\['
+t_COLCH_DIR = r'\]'
+t_PONTO_VIRGULA = r';'
+t_VIRGULA = r','
+t_PONTO = r'\.'
 
-class Token:
-    def __init__(self, type, value, line, column):
-        self.type = type
-        self.value = value
-        self.line = line
-        self.column = column
-
-    def token(self):
-        return (self.type, self.value)
+# Ignorar espaços em branco e tabulações
+t_ignore = ' \t'
 
 
-class Lexer:
-    def __init__(self, code):
-        self.code = code
-        self.position = 0
-        self.line = 1
-        self.column = 1
+def t_COMMENT(t):
+    r'//.*'
+    pass
 
-    def tokenize(self):
-        tokens = []
-        while self.position < len(self.code):
-            token = self._next_token()
-            if token:
-                tokens.append(token)
-        return tokens
 
-    def _next_token(self):
-        if self.position >= len(self.code):
-            return None
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
-        for type, pattern in TOKEN_TYPES.items():
-            regex = re.compile(pattern)
-            match = regex.match(self.code, self.position)
 
-            if match:
-                value = match.group(0)
-                token = Token(type, value.replace("\"", ""), self.line, self.column)
-                self._update_position(value)
+def t_error(t):
+    print(f"Caractere inválido '{t.value[0]}' na linha {t.lineno}")
+    t.lexer.skip(1)
 
-                if type == 'WHITESPACE' or type == 'COMMENT':
-                    return self._next_token()
-
-                return token.token()
-
-        invalid_char = self.code[self.position]
-        raise SyntaxError(f'Caractere inválido "{invalid_char}" na linha {self.line}, coluna {self.column}')
-
-    def _update_position(self, value):
-        self.position += len(value)
-        if '\n' in value:
-            self.line += value.count('\n')
-            self.column = 1
-        else:
-            self.column += len(value)
-
+lexer = lex.lex()
 
 def main(filename):
     with open(filename, 'r') as file:
         code = file.read()
 
-    lexer = Lexer(code)
-    tokens = lexer.tokenize()
-
-    for token in tokens:
-        if token[0] in showed_types:
-            print(f"<{token[0]}, {token[1]}>")
-        else:
-            print(f"<{token[0]}>")
-
+    lexer.input(code)
+    for token in lexer:
+        print(f"<{token.type}, {token.value}>")
 
 if __name__ == "__main__":
+    import sys
     if len(sys.argv) != 2:
-        print("Uso: python script.py <filename>")
+        print("Uso: python lexer.py <filename>")
     else:
         main(sys.argv[1])
